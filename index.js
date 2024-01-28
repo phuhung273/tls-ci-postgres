a44879c35bfbe669befeb8658429c4b32f9663d9
phuhung273 <phuhung.tranpham@gmail.com>
2024-01-28 21:54:12 +0700

    use node env

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const fs = require('fs')
 
 const config = {
     connectionString: 'postgres://postgres:password@localhost:5432/postgres?sslmode=require',
-    ssl: {
-        ca: fs.readFileSync('cacert.pem'),
-    },
 }
 
 const client = new Client(config);
