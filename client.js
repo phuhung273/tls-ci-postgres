c33476663846d86731481707006d9e4bcbbbca5a
phuhung273 <phuhung.tranpham@gmail.com>
2024-01-28 20:57:14 +0700

    finish example

--- client.js
+++ client.js
@@ -1,16 +1,6 @@
-const https = require('https')
-const fs = require('fs')
-
-const ca = fs.readFileSync('cacert.pem');
-
-console.log(ca);
 
 (async function () {
-
-
-    let response = await fetch("https://localhost:2443", {
-        agent: new https.Agent({ ca: ca })
-    });
+    let response = await fetch("https://localhost:2443");
     let fetchHtml = await response.text();
     console.log(fetchHtml)
 })()
